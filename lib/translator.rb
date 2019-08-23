91a747163f56d5f10b683caea55492ab936742f5
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-23 02:53:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,19 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(library)
+  lib = {:get_emoticon => {}, :get_meaning => {}}
+  YAML.load_file(library).keys.each do |emo|
+    japamote = YAML.load_file(library)[emo][1]
+    lib[:get_meaning][japamote] = emo
+    engmote = YAML.load_file(library)[emo][0]
+    lib[:get_emoticon][engmote] = japamote
+  end
+  return lib
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(library, emoticon)
+  lib = load_library(library)
+  emote = lib[:get_emoticon][emoticon]
 end
 
 def get_english_meaning
